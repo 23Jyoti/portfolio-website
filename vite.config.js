7337be754e240ecdc60307ca43a0cbcb32043d80
Jyoti Gaud <23jyotigaud@gmail.com>
2025-04-18 16:48:23 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: procees.env.VITE_BASE-PATH ||"/portfolio-website",
   css: {
     modules: {
       localsConvention: "camelCase",
